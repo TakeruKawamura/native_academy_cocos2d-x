a5baee610dc051b1bac205a564b0b63a6274dfeb
cocos2d-x <cocos2d-x@certpro115.local>
2014-02-02 16:31:54 +0900

    結果画面に音追加

--- tower/Classes/GameResult.cpp
+++ tower/Classes/GameResult.cpp
@@ -8,8 +8,10 @@
 //
 
 #include "GameResult.h"
+#include "SimpleAudioEngine.h"
 
 USING_NS_CC;
+using namespace CocosDenshion;
 
 CCScene* GameResult::scene()
 {
@@ -26,13 +28,10 @@ CCScene* GameResult::scene()
     return scene;
 }
 
-// on "init" you need to initialize your instance
 bool GameResult::init()
 {
     CCLOG("call init");
     
-    //////////////////////////////
-    // 1. super init first
     if ( !CCLayer::init() )
     {
         return false;
@@ -41,53 +40,20 @@ bool GameResult::init()
     CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
     CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
     
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-    
-    // add a "close" icon to exit the progress. it's an autorelease object
-    /*
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                                          "skull1.png",
-                                                          "skull2.png",
-                                                          this,
-                                                          menu_selector(GameResult::menuCloseCallback));
-    
-	pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/10,
-                                origin.y + pCloseItem->getContentSize().height/10));
-    
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition(CCPointZero);
-    this->addChild(pMenu, 1);
-    */
-    /////////////////////////////
-    // 3. add your codes below...
-    
-    // add a label shows "Hello World"
-    // create and initialize a label
-    /*
-    CCLabelTTF* pLabel = CCLabelTTF::create("GAME OVER", "Arial", 50);
-    
-    // position the label on the center of the screen
-    pLabel->setPosition(ccp(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height*3/4));
-    
-    // add the label as a child to this layer
-    this->addChild(pLabel, 1);
-    */
-    // add "HelloWorld" splash screen"
     CCSprite* pSprite1 = CCSprite::create("grave.png");
     
-    // position the sprite on the center of the screen
     pSprite1->setPosition(ccp(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
     
-    // add the sprite as a child to this layer
     this->addChild(pSprite1, 0);
-
     this->addGameover();
     this->scheduleOnce(schedule_selector(GameResult::showScore), 2.0f);
     
+    // BGM、効果音のロード
+    SimpleAudioEngine::sharedEngine()->setBackgroundMusicVolume(0.5);
+    SimpleAudioEngine::sharedEngine()->preloadBackgroundMusic("sound/gameover_bgm.mp3");
+    SimpleAudioEngine::sharedEngine()->preloadEffect("sound/gameover.mp3");
+    SimpleAudioEngine::sharedEngine()->playBackgroundMusic("sound/gameover_bgm.mp3");
+
     return true;
 }
 
@@ -116,22 +82,15 @@ void GameResult::addGameover()
                    CCSequence::create(
                                       actionMove1,
                                       actionMove2,
-                                      //GameResult::showScore(),
                                       NULL
                                       )
                    );
-    //this->schedule(schedule_selector(GameResult::moveGameover));
-}
-
-void GameResult::moveGameover()
-{
-    CCLOG("call moveGameover");
-    
-    //ここに記入されたモノを、定期的に呼び出す
 }
 
 void GameResult::showScore()
 {
+    SimpleAudioEngine::sharedEngine()->playEffect("sound/gameover.mp3");
+
     CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
     CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
     
@@ -150,16 +109,3 @@ void GameResult::showScore()
     this->addChild(pLabel1, 1);
     this->addChild(pSprite1, 1);
 }
-
-void GameResult::menuCloseCallback(CCObject* pSender)
-{
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
-	CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-#else
-    CCDirector::sharedDirector()->end();
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-#endif
-}
-
